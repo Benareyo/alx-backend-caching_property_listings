55d827a0c4cbe62c03320ad113c06bacae832902
Benareyo <betelyohannes39@gmail.com>
2025-08-29 12:52:43 -0700

    adding changes

--- properties/apps.py
+++ properties/apps.py
@@ -1,6 +1,10 @@
 from django.apps import AppConfig
 
 
-class PropertiesConfig(AppConfig):
+class PropertyAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'properties'
+    
+    def ready(self):
+        # Import signals when the app is ready
+        import properties.signals
